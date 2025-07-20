d7ee285b6789b4a8473d29e1e572b857b4bad4d7
Sherine <135027428+108405042@users.noreply.github.com>
2025-07-20 14:41:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ import io
 import datetime
 
 # UI：標題與輸入框
-st.title("🏅 陽明交大研究感謝狀")
+st.title("🏅 陽明交大研究總結獎狀")
 name = st.text_input("請輸入您的名字：", "")
 
 if name:
     # 載入底圖與字型
-    bg = Image.open("2.png").convert("RGBA")
+    bg = Image.open("4.png").convert("RGBA")
     font = ImageFont.truetype("NotoSerifTC-VariableFont_wght.ttf", size=80)
     draw = ImageDraw.Draw(bg)
 
     # 將名字置中寫上
     bbox = draw.textbbox((0, 0), name, font=font)
     text_width = bbox[2] - bbox[0]
-    x = (bg.width - text_width) / 2
-    y = 580  # 視你的圖設計，調整 Y 軸
+    x = 1135
+    y = 680  # 視你的圖設計，調整 Y 軸
     draw.text((x, y), name, fill="black", font=font)
 
     # 顯示預覽
-    st.image(bg, caption="🎉 研究有您參與真好，恭喜您獲得獎狀！", use_container_width=True)
+    st.image(bg, caption="🎉 恭喜您走過21天的研究旅程，獲得獎狀！", use_container_width=True)
 
     # 提供下載按鈕
     buf = io.BytesIO()
@@ -29,6 +29,6 @@ if name:
     st.download_button(
         label="📥 下載我的獎狀",
         data=buf.getvalue(),
-        file_name=f"第二週感謝狀_{name}.png",
+        file_name=f"總結感謝狀_{name}.png",
         mime="image/png"
     )
